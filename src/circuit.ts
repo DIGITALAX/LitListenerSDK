d155c20a9b1bc1ada98afec1024f36da90be58de
emmajane1313 <admin@digitalax.xyz>
2023-07-21 16:33:54 -0400

    add log emitter

--- src/circuit.ts
+++ src/circuit.ts
@@ -1204,6 +1204,6 @@ export class Circuit extends EventEmitter {
 
     this.logs[this.logIndex] = { category, message, responseObject };
     this.logIndex = (this.logIndex + 1) % this.logSize;
-    this.emit("log", message);
+    this.emitter.emit("log", message);
   };
 }
